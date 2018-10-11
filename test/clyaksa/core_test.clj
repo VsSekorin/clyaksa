c2df62027410eb053cefc64c9da6eb4300daae72
Vseslav Sekorin <VsSekorin@gmail.com>
2018-10-11 21:33:47 +0300

    new image

--- test/clyaksa/core_test.clj
+++ test/clyaksa/core_test.clj
@@ -1,7 +1,23 @@
 (ns clyaksa.core-test
   (:require [clojure.test :refer :all]
-            [clyaksa.core :refer :all]))
+            [clyaksa.core :refer :all])
+  (:import (java.awt.image BufferedImage)))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest new-image-size-test
+  (testing "new image return with correct size"
+    (let [size 100
+          image (new-image size :rgb)]
+      (is (= (class image) BufferedImage))
+      (is (= (.getWidth image) size))
+      (is (= (.getHeight image) size))
+      (is (= (.getType image) BufferedImage/TYPE_INT_RGB)))))
+
+(deftest new-image-width-height-test
+  (testing "new image return with correct size"
+    (let [width 200
+          height 300
+          image (new-image width height :argb)]
+      (is (= (class image) BufferedImage))
+      (is (= (.getWidth image) width))
+      (is (= (.getHeight image) height))
+      (is (= (.getType image) BufferedImage/TYPE_INT_ARGB)))))
