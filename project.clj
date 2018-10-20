ee6e748f4345bfa0d020f8bf8e7e8a4c22c9a472
Vseslav Sekorin <VsSekorin@gmail.com>
2018-10-20 15:42:39 +0300

    change project.clj for pre-release

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject clyaksa "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Simple library for pixel art"
+  :url "vssekorin.com"
+  :uberjar-name "clyaksa.jar"
   :dependencies [[org.clojure/clojure "1.8.0"]])
